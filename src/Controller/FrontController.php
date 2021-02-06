4abdb2fac2fdc0b831e4648d517844d0b1d50e68
Nerpp <regiaurelien@gmail.com>
2021-02-06 18:44:49 +0100

    correction date comments

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -9,12 +9,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
 {
+
+    
     /**
      * @Route("/", name="front_index")
      */
     public function index(TrickRepository $tricksRepository)
     {
-        
         return $this->render('front/index.html.twig', [
             'tricks' => $tricksRepository->findBy(array(),array('id'=> 'ASC'),$limit=10,$offset=null),
         ]);
