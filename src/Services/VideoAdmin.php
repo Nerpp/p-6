8bffb75b247962e0d2ac887db1f1f987e0b70ea5
Nerpp <regiaurelien@gmail.com>
2021-05-02 16:34:14 +0200

    correction video admin deleting else

--- src/Services/VideoAdmin.php
+++ src/Services/VideoAdmin.php
@@ -5,15 +5,10 @@ class VideoAdmin
 {
     public function addEmbed(string $var)
     {
-       
-        if (preg_match("/\bembed\b/i",$var)) {
+        if (preg_match("/\bembed\b/i", $var)) {
             return $var;
-        } else { 
-            
-            parse_str( parse_url( $var, PHP_URL_QUERY ), $my_array );
-            
-            return'https://www.youtube.com/embed/'.$my_array['v'];
         }
-        
+        parse_str( parse_url( $var, PHP_URL_QUERY ), $my_array );
+        return'https://www.youtube.com/embed/'.$my_array['v'];
     }
 }
