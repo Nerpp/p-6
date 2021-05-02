4f80e004ff251ebba5ceaa0cacf1a28bf893878e
Nerpp <regiaurelien@gmail.com>
2021-05-02 14:11:28 +0200

    correction registration controller deleting else on image && deleting space

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,20 +5,17 @@ namespace App\Controller;
 use App\Entity\Images;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
-use App\Security\CustomAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Core\Security;
 use App\Repository\UserRepository;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-
 
 class RegistrationController extends AbstractController
 {
@@ -45,26 +42,20 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
             $mail = $form->get('email')->getData();
-
-
             $pattern = "#[?,;./:§!%µ*¨^£$\¤{}()[\]\-\|`_\\@&~\#]#";
 
             if (!preg_match($pattern, $form->get('plainPassword')->getData())) {
-
                 $this->addFlash('failed', 'Special characters must be used  !');
                 return $this->redirectToRoute('app_register');
             }
 
             if (preg_match($pattern, $form->get('name')->getData())) {
-
                 $this->addFlash('failed', 'Special characters must not be used  !');
                 return $this->redirectToRoute('app_register');
             }
 
             if (preg_match($pattern, $form->get('surname')->getData())) {
-
                 $this->addFlash('failed', 'Special characters must not be used  !');
                 return $this->redirectToRoute('app_register');
             }
@@ -77,9 +68,7 @@ class RegistrationController extends AbstractController
                 )
             );
             $imageFile = $form->get('image')->getData();
-
             $image = new Images();
-
             if ($imageFile) {
                 $newFilename = uniqid() . '.' . $imageFile->guessExtension();
                 try {
@@ -93,26 +82,22 @@ class RegistrationController extends AbstractController
                 }
                 $image->setSource($newFilename);
                 
-            }else{
+            }
+
+            if(!$imageFile){
                 $image->setSource('defaultAvatar.jpg');
             }
 
             $user->setImages($image);
-
             $validation = random_int(100000, 9999999);
             $hash = md5($validation);
             $user->setValidationToken($hash);
 
             $email = (new TemplatedEmail())
                 ->from('smptpserveur@gmail.com')
-                // ->to(new Address('ryan@example.com'))
                 ->to($mail)
                 ->subject('Thanks for signing up!')
-
-                // path of the Twig template to render
                 ->htmlTemplate('emails/signup.html.twig')
-
-                // pass variables (name => value) to the template
                 ->context([
                     'expiration_date' => new \DateTime('+2 days'),
                     'username' => $form->get('name')->getData(),
@@ -129,13 +114,10 @@ class RegistrationController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
-
             $this->addFlash('success', 'Congrat an user is born, would you check your email for confirm it !');
-
             return $this->redirectToRoute('front_index');
     
         }
-
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
@@ -148,8 +130,6 @@ class RegistrationController extends AbstractController
     public function confirmPass($token, UserRepository $users,Request $request):Response
     {
         $user = $users->findOneBy(['validationToken' => $token]);
-
-
         if(!$user){
             // On renvoie une erreur 404
             throw $this->createNotFoundException('Unknow user');
@@ -160,19 +140,15 @@ class RegistrationController extends AbstractController
         $interval = (int)$interval->format('%R%a');
 
         if ($interval >> 2) {
-
             throw $this->createNotFoundException('Bloody fate, the confirmation is out of concern, can you try again !');
         }
 
         $user->setValidationToken(null);
         $user->setValidation(1);
-
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->persist($user);
         $entityManager->flush();
-
         $this->addFlash('success', 'Congrats that was the last step, Welcome among us  !');
-        
         return $this->redirectToRoute('app_login');
     }
 }
