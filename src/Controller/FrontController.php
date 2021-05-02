c04ce5f9b67bd1f9d5abeab84d05fd7691a13146
Nerpp <regiaurelien@gmail.com>
2021-05-02 16:02:16 +0200

    correction front controller variable

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -10,9 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 class FrontController extends AbstractController
 {
-    protected $iLength;
-    protected $iBdd;
-
+  
     public function __construct()
     {
         $this->pagination = new Pagination;
@@ -23,12 +21,12 @@ class FrontController extends AbstractController
      */
     public function index(TrickRepository $tricksRepository)
     {
-        $this->iBdd = count($tricksRepository->findAll());
-        $this->iLength = $this->pagination->tricksPagination(0, $this->_iBdd);
+        $iBdd = count($tricksRepository->findAll());
+        $iLength = $this->pagination->tricksPagination(0, $iBdd);
 
         return $this->render('front/index.html.twig', [
-            'tricks' => $tricksRepository->findBy(array(), array('id' => 'ASC'), $this->_iLength, null),
-            'pagination' => $this->_iBdd,
+            'tricks' => $tricksRepository->findBy(array(), array('id' => 'ASC'), $iLength, null),
+            'pagination' => $iBdd,
         ]);
     }
 
@@ -37,13 +35,13 @@ class FrontController extends AbstractController
      */
     public function pagination(TrickRepository $tricksRepository, Request $request)
     {
-        $this->iBdd = count($tricksRepository->findAll());
+        $iBdd = count($tricksRepository->findAll());
         $paging = $request->query->get('length');
-        $this->iLength = !is_null($paging) ? $this->pagination->tricksPagination($paging, $this->_iBdd) : $this->pagination->tricksPagination(0, $this->_iBdd);
+        $iLength = !is_null($paging) ? $this->pagination->tricksPagination($paging, $iBdd) : $this->pagination->tricksPagination(0, $iBdd);
 
         return $this->render('front/index.html.twig', [
-            'tricks' => $tricksRepository->findBy(array(), array('id' => 'ASC'), $this->_iLength, null),
-            'pagination' => $this->_iBdd,
+            'tricks' => $tricksRepository->findBy(array(), array('id' => 'ASC'), $iLength, null),
+            'pagination' => $iBdd,
         ]);
     }
 }
